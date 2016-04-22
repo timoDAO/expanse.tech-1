98f50b065f6132303d56c2b5ab4b40ad7d200cc9
Dan Conway <surreal7@gmail.com>
2016-04-22 18:03:52 -0500

    Fixed Various Header Bugs
    Fixed stickyheader not showing if page loaded on hash
    Fixed header link javascript error on subpages

--- js/expanse_main.js
+++ js/expanse_main.js
@@ -1,65 +1,62 @@
-	var marginOffset=50;
-	var navOffset=marginOffset+15;
+var marginOffset=50;
+var navOffset=marginOffset+15;
+var navItems = []; 
 function goToByScroll(id){
-
     $('html,body').animate({scrollTop: $(id).offset().top-marginOffset} , 1500, 'easeInOutExpo');
 }
 
 $(".navlink, .docslink").click(function(e) {
-	e.preventDefault();
-    goToByScroll(this.hash);           
+	if($(this.hash).length>0){
+		e.preventDefault();
+    	goToByScroll(this.hash);    
+	}
 });
 
 $("#read_more").click(function(e) {
 	e.preventDefault();
     goToByScroll("#about");           
 });
-
+var initialOffset =  $('#navbar1').offset().top;
+function scrollCheck(){
+	var scrolled = $(window).scrollTop();
+	var windowHeight = $(window).height(); 
+	
+	/*sticky header*/
+	if (scrolled >initialOffset) {
+		 $('#navbar').removeClass('navbar-fixed-top').addClass('navbar-fixed-top');
+		 $('.navbar-header').hide();
+		 $('.navspacer').show();
+	} else {
+		$('#navbar').removeClass('navbar-fixed-top');
+		$('.navbar-header').show();
+		$('.navspacer').hide();
+	}
+	
+	// auto highlight nav elements if section is currently primary in frame
+	for (var i=0; i < navItems.length; i++) {
+		if($(navItems[i]).length>0){
+			var divPos = $(navItems[i]).offset().top;
+			var divHeight = $(navItems[i]).height();
+			if (scrolled >= divPos-navOffset && scrolled < (divPos + divHeight)+navOffset+15) {
+				$("a[href='/index.php" + navItems[i] + "']").addClass("nav-active");
+			} else {
+				$("a[href='/index.php" + navItems[i] + "']").removeClass("nav-active");
+			}
+		}
+	}
+	
+}
 jQuery(document).ready(function($) {
-
-	var initialOffset =  $('#navbar1').offset().top;
 	var navlinks = $(".navlink"); 
-    var navItems = []; 
+  
     for (var i=0; i < navlinks.length; i++) {    
         var aChild = navlinks[i];
         var href = $(aChild).attr('href');
 		var hash = href.substr(href.indexOf("#"));
-		//alert(hash);
 		if(hash.length>0) navItems.push(hash);
     } 
-	$('.mini_logo').hide();
 	$(window).scroll(function() {
-		// sticky header
-		
-		var scrolled = $(window).scrollTop();
-		if (scrolled >initialOffset) {
-			 $('#navbar').removeClass('navbar-fixed-top').addClass('navbar-fixed-top');
-			 $('.navbar-header').hide();
-			 $('.navspacer').show();
-
-
-		} else {
-			$('#navbar').removeClass('navbar-fixed-top');
-			$('.navbar-header').show();
-			$('.navspacer').hide();
-
-
-		}
-		
-		// auto highlight nav elements if section is currently primary in frame
-		var windowPos = $(window).scrollTop(); 
-		var windowHeight = $(window).height(); 
-		for (var i=0; i < navItems.length; i++) {
-			if($(navItems[i]).length>0){
-				var divPos = $(navItems[i]).offset().top;
-				var divHeight = $(navItems[i]).height();
-				if (windowPos >= divPos-navOffset && windowPos < (divPos + divHeight)+navOffset+15) {
-					$("a[href='/index.php" + navItems[i] + "']").addClass("nav-active");
-				} else {
-					$("a[href='/index.php" + navItems[i] + "']").removeClass("nav-active");
-				}
-			}
-		}
-
+		scrollCheck();
 	});	
+	scrollCheck();
 });
