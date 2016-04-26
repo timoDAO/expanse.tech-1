32d4a7fb38ced1e64acdff6c22700964985dfb40
Timothée <timothee.deretz@gmail.com>
2016-04-27 00:46:44 +0200

    Update global.php

--- i8n/fr/global.php
+++ i8n/fr/global.php
@@ -1,1 +1,15 @@
-// test2
+<?php
+//Expanse Internationalzation Language File - Français : Global 
+//Version: 1.1.0
+//Global : General
+$i8n['global_title']='Expanse | Plateforme Blockchain Libre';
+// Global : Navigation
+$i8n['nav_screenread']='Basculer Navigation';
+$i8n['nav_slogan']='It\'s for everyone.';
+$i8n['nav_about']='À propos';
+$i8n['nav_downloads']='Téléchargements';
+$i8n['nav_documentation']='Documentation';
+$i8n['nav_team']='Équipe';
+$i8n['nav_community']='Communauté';
+$i8n['nav_explorer']='Block Explorer';
+?>
