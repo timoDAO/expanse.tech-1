ebcf2bc402664954abcc96f542cdfc3e0773f8b8
Kanopt <maria.c.pereira1@outlook.pt>
2016-05-29 19:52:09 +0100

    Final Portuguese translation

--- i8n/pt/global.php
+++ i8n/pt/global.php
@@ -3,15 +3,15 @@
 //Version: 0.0.1
 
 //Global : General
-$i8n['global_title']='Expanse | Open Blockchain Platform';
+$i8n['global_title']='Expanse | Plataforma aberta de Blockchain';
 
 // Global : Navigation
-$i8n['nav_screenread']='Toggle navigation';
-$i8n['nav_slogan']='It\'s for everyone.';
-$i8n['nav_about']='About';
+$i8n['nav_screenread']='Alternar Navegação';
+$i8n['nav_slogan']='É para toda a gente.';
+$i8n['nav_about']='Sobre';
 $i8n['nav_downloads']='Downloads';
-$i8n['nav_documentation']='Documentation';
-$i8n['nav_team']='Team';
-$i8n['nav_community']='Community';
-$i8n['nav_explorer']='Block Explorer';
+$i8n['nav_documentation']='Documentação';
+$i8n['nav_team']='Equipa';
+$i8n['nav_community']='Comunidade';
+$i8n['nav_explorer']='Explorador de Blocos';
 ?>
